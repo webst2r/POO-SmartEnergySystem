bd72daea957823167882369b8c92119d89339b7e
webst2r <rodrigorodrigues56@hotmail.com>
2022-04-07 17:26:02 +0100

    SmartCamera

--- SmartEnergySystem/src/SmartCamera.java
+++ SmartEnergySystem/src/SmartCamera.java
@@ -4,4 +4,32 @@ public class SmartCamera extends SmartDevice{
     private double consumo;
 
 
+    public SmartCamera(){
+        this.resolucao = "";
+        this.tamanhoFicheiro = 0.0;
+        this.consumo = 0.0;
+    }
+
+    public SmartCamera(String id, Modo modo, String resolucao, double tamanhoFicheiro, double consumo){
+        super (id, modo);
+        this.resolucao = resolucao;
+        this.tamanhoFicheiro = tamanhoFicheiro;
+        this.consumo = consumo;
+    }
+
+    public SmartCamera(SmartCamera sc){
+        super (sc.getID(), sc.getModo());
+        this.resolucao = sc.getResolucao();
+        this.tamanhoFicheiro = sc.getTamanhoFicheiro();
+        this.consumo = sc.getConsumo();
+    }
+
+    public String getResolucao() { return this.resolucao;}
+    public double getTamanhoFicheiro() { return this.tamanhoFicheiro; }
+
+    public double getConsumo() { return this.consumo;}
+
+    public SmartCamera clone() {
+        return new SmartCamera(this);
+    }
 }
