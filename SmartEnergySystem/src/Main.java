e82ed69ca4d334f9f728a07f7c9d722bd575df42
webst2r <rodrigorodrigues56@hotmail.com>
2022-05-03 14:54:24 +0100

    Load & Save state from binary files

--- SmartEnergySystem/src/Main.java
+++ SmartEnergySystem/src/Main.java
@@ -4,8 +4,10 @@ import Utilities.LinhaIncorretaException;
 import Utilities.Parser;
 import View.View;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws LinhaIncorretaException {
+    public static void main(String[] args) throws LinhaIncorretaException, IOException, ClassNotFoundException {
 
         Model model = new Model();
         View view = new View();
